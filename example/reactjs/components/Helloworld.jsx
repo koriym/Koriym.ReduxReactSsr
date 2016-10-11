83d67cb76979b6c72790ac6ba0270d4fdae2fb41
Akihito Koriyama <akihito.koriyama@gmail.com>
2016-10-11 17:46:59 +0900

    add onClick handler

--- example/reactjs/components/Helloworld.jsx
+++ example/reactjs/components/Helloworld.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
-const HelloWorld = props => (
-  <div>
-    Hello {props.name}
-  </div>
-);
+class HelloWorld extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: this.props.name };
+  }
+  _handleClick() {
+    this.setState({ name: 'CSR'});
+  }
+  render() {
+    return (
+        <div>
+          <div>Hello {this.state.name}</div>
+          <button onClick={ this._handleClick.bind(this) }>Click</button>
+        </div>
+    )
+  }
+}
 
 HelloWorld.propTypes = {
   name: React.PropTypes.string.isRequired,
