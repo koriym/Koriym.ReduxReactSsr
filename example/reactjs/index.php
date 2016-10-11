7167e8ace77b88a1eb099d7c6e430a6407d8d4d9
Akihito Koriyama <akihito.koriyama@gmail.com>
2016-10-12 02:57:35 +0900

    echo missing js

--- example/reactjs/index.php
+++ example/reactjs/index.php
@@ -1,7 +1,6 @@
 <?php
 require dirname(__DIR__, 2) . '/vendor/autoload.php';
 
-
 $reactJs = new \ReactJS(
     file_get_contents(__DIR__ . '/build/react.bundle.js'),
     file_get_contents(__DIR__ . '/build/helloworld.bundle.js')
@@ -13,11 +12,19 @@ $js = $reactJs->getJS('#root');
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Hello SSR</title>
+    <title>Hello ReactJS-SSR</title>
   </head>
   <body>
+    <!-- render server content here -->
     <div id="root"><?php echo $html; ?></div>
+    <!-- load react and app code -->
     <script src="build/react.bundle.js"></script>
     <script src="build/helloworld.bundle.js"></script>
+    // client init/render
+    // this is a straight echo of the JS because the JS resources
+    // were loaded synchronously
+    // You may want to load JS async and wrap the return of getJS()
+    // in a function you can call later
+    <script><?php echo $js; ?></script>
   </body>
 </html>
