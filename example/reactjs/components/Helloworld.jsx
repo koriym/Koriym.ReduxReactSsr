73608d937b2f365f8e76cfa3ea6dc411f81c198a
Akihito Koriyama <akihito.koriyama@gmail.com>
2016-10-12 17:47:33 +0900

    use static propTypes / defaultProps with stage-0

--- example/reactjs/components/Helloworld.jsx
+++ example/reactjs/components/Helloworld.jsx
@@ -1,31 +1,29 @@
 import React from 'react';
 
 class HelloWorld extends React.Component {
+  static propTypes = {
+    name: React.PropTypes.string.isRequired,
+  }
+  static defaultProps = {
+    name: 'World',
+  }
   constructor(props) {
     super(props);
     this.state = {
       name: this.props.name
     };
   }
-  _handleClick() {
+  handleClick() {
     this.setState({ name: 'CSR'});
   }
   render() {
     return (
         <div>
           <div>Hello {this.state.name}</div>
-          <button onClick={ this._handleClick.bind(this) }>Click</button>
+          <button onClick={ this.handleClick.bind(this)}>Click</button>
         </div>
     )
   }
 }
-
-HelloWorld.propTypes = {
-  name: React.PropTypes.string.isRequired,
-};
-
-HelloWorld.defaultProps = {
-  name: 'World',
-};
 
 export default HelloWorld;
