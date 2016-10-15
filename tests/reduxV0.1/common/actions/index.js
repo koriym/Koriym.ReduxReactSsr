65aee69d4663bc58b623f1226a78b973010b9729
Akihito Koriyama <akihito.koriyama@gmail.com>
2016-10-15 09:26:15 +0900

    add old version(v0.1) sample code for for legacy test

---
+++ tests/reduxV0.1/common/actions/index.js
@@ -0,0 +1,13 @@
+/*
+ * action types
+ */
+export const HELLO_WORLD = 'HELLO_WORLD'
+
+/*
+ * action creators
+ */
+export const helloWorld = () => {
+  return {
+    type: HELLO_WORLD
+  }
+}
---
+++ tests/reduxV0.1/common/app.js
@@ -0,0 +1,3 @@
+import App from './components/App';
+
+global.App = App;
